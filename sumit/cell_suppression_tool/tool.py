77627e01423792bdfe39a68678807db6f359f743
Richard Preen <rpreen@gmail.com>
2022-10-27 23:26:01 +0100

    add passing arguments

--- sumit/cell_suppression_tool/tool.py
+++ sumit/cell_suppression_tool/tool.py
@@ -11,9 +11,17 @@ import subprocess
 from pathlib import Path
 
 
-def run() -> None:
+def run(args: list[str]) -> None:
+    """Runs the cell suppression tool.
+
+    Parameters
+    ----------
+    args : list[str]
+        Command line arguments.
+    """
     exe = "cell_suppression_tool"
     path = Path(__file__).parent
     if platform.system() == "Windows":
         exe += ".exe"
-    subprocess.run([os.path.join(path, exe)])
+    command = [os.path.join(path, exe)] + args
+    subprocess.run(command, check=True)
